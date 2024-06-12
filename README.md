135ef1f015cbe6e162e6c248cc3bf78635790337
Lincon Venicios Gallo dos Santos <lincon@MacBook-Pro-de-Raquel.local>
2024-06-12 09:31:23 -0300

    [06/12/2024] - [0.15.0] - Updated Packages: 1. FEATURE: Message reception channels were included, in addition to improvements in documentation

---
+++ README.md
@@ -0,0 +1,17 @@
+# Chat Server Socketio
+
+## Description
+
+Websockets server, developed for exchanging messages in a chat
+
+## Requirements
+
+- NodeJs v20.14.0
+- NPM or Yarn (We recommend using yarn)
+- TypeScript, ts-node and nodemon ( ``npm install -g typescript ts-node nodemon`` )
+
+## How to test?
+
+I developed a flutter client that will be available for clone with the link below.
+
+[https://github.com/linconDev/chat-client-socket-flutter]
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import "dotenv/config";
 import { createServer } from "http";
 import { Server } from "socket.io";
 import { DefaultEventsMap } from "socket.io/dist/typed-events";
@@ -6,7 +7,19 @@ const httpServer = createServer();
 const io: Server<DefaultEventsMap, any> = new Server(httpServer, {});
 
 io.on("connection", (socket) => {
+  console.log("a user connected", socket);
 
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+
+  socket.on("chat message", (msg) => {
+    console.log('Message Received: ' + msg)
+    io.emit("chat message", msg);
+  });
 });
 
-io.listen(3000);
+const port = process.env.PORT || 3000;
+httpServer.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
