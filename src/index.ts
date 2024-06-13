69fee89ad8b080f62785276d0f33b23cf50a9c29
Lincon Venicios Gallo dos Santos <lincon@MacBook-Pro-de-Raquel.local>
2024-06-12 22:53:21 -0300

    [06/12/2024] - [0.20.0] - Updated Packages: 1. The server is taking shape in terms of exchanging private messages between users, with the inclusion of individual rooms, which use the ID of the user's connection to the socket.

--- src/index.ts
+++ src/index.ts
@@ -6,16 +6,28 @@ import { DefaultEventsMap } from "socket.io/dist/typed-events";
 const httpServer = createServer();
 const io: Server<DefaultEventsMap, any> = new Server(httpServer, {});
 
-io.on("connection", (socket) => {
-  console.log("a user connected", socket);
+const users: any = {};
 
-  socket.on("disconnect", () => {
-    console.log("user disconnected");
+io.on("connection", (socket) => {
+  console.log("a user connected");
+
+  socket.on("join", (userName) => {
+    console.log(`${userName} joined the chat`);
+    users[socket.id] = userName;
+    socket.join(userName);
+    io.emit("user list", Object.values(users));
   });
 
-  socket.on("chat message", (msg) => {
-    console.log('Message Received: ' + msg)
-    io.emit("chat message", msg);
+  socket.on("private message", ({ sender, recipient, message }) => {
+    console.log(`Message from ${sender} to ${recipient}: ${message}`);
+    io.to(recipient).emit("chat message", `${sender}: ${message}`);
+  });
+
+  socket.on("disconnect", () => {
+    const userName = users[socket.id];
+    console.log(`${userName} disconnected`);
+    delete users[socket.id];
+    io.emit("user list", Object.values(users));
   });
 });
 
